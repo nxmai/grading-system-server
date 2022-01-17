8b3fab7de9a6a308f0c5b2af953a2e112ba18b34
phamvmnhut <phamvmnhut@gmail.com>
2022-01-17 16:32:41 +0700

    class: search index

--- components/class/classModel.js
+++ components/class/classModel.js
@@ -24,14 +24,37 @@ const ClassSchema = mongoose.Schema(
             type: String,
             select: false,
         },
+        description__search: {
+            type: String,
+            select: false,
+        },
     },
     {
         timestamps: true,
     }
 );
 
+ClassSchema.index({ name__search: 'text', description__search: 'text'});
+
 ClassSchema.pre('save', async function (next) {
     if (this.name) this.name__search = convVie(this.name).toLowerCase();
+    if (this.description) this.description__search = convVie(this.description).toLowerCase();
+    return next();
+});
+
+ClassSchema.pre(/findOneAndUpdate|updateOne|update/, function (next) {
+    const docUpdate = this.getUpdate();
+    // return if not update search
+    if (!docUpdate) return next();
+    const updateDocs = {};
+    if (docUpdate.name) {
+        updateDocs.name__search = convVie(docUpdate.name).toLowerCase();
+    }
+    if (docUpdate.description) {
+        updateDocs.description__search = convVie(docUpdate.description).toLowerCase();
+    }
+    // update
+    this.findOneAndUpdate({}, updateDocs);
     return next();
 });
 
